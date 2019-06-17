4ae4c12623f69bf78f3d4ef961ea6d9267551af8
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-17 07:05:41 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,13 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
+      item = Item.find_by(name: item_name)
+      if item
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        req.status = 400
+      end
     else
       resp.write "Route not found"
       req.status = 404
