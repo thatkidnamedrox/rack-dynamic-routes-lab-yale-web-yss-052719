1e9b42f623d16a03566b17e0c2cd6b2be4c96230
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-17 07:06:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,11 +10,11 @@ class Application
         resp.write item.price
       else
         resp.write "Item not found"
-        req.status = 400
+        resp.status = 400
       end
     else
       resp.write "Route not found"
-      req.status = 404
+      resp.status = 404
     end
     resp.finish
   end
