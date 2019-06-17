81c3c6cbb67e8fe047572d687a7abdfd3364cbde
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-17 07:04:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item = req.path.split("/items/").last
+      item_name = req.path.split("/items/").last
     else
       resp.write "Route not found"
       req.status = 404
